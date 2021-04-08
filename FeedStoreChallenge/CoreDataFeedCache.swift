978a21b68cf4974e908ad085a3db2c8b2f3f5577
Maciej Krolikowski <maciej.krolikowski@allegro.pl>
2021-04-08 22:15:07 +0200

    Merge CoredataFeedCache generated files into one

---
+++ FeedStoreChallenge/CoreDataFeedCache.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataFeedCache.swift
+//  FeedStoreChallenge
+//
+//  Created by Maciej Krolikowski on 08/04/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataFeedCache)
+public class CoreDataFeedCache: NSManagedObject {
+	@NSManaged public var timestamp: Date
+	@NSManaged public var feedItems: NSOrderedSet
+}
+
+extension CoreDataFeedCache {
+	static func deleteRequest() -> NSBatchDeleteRequest {
+		return NSBatchDeleteRequest(fetchRequest: CoreDataFeedCache.fetchRequest())
+	}
+
+	static func insert(feed: [LocalFeedImage], timestamp: Date, context: NSManagedObjectContext) {
+		let cache = self.init(context: context)
+		cache.feedItems = NSOrderedSet(array: feed.map { $0.toEntity(context: context) })
+		cache.timestamp = timestamp
+	}
+
+	static func fetch(context: NSManagedObjectContext) throws -> CoreDataFeedCache? {
+		let result = try context.fetch(CoreDataFeedCache.fetchRequest())
+		return result.first as? CoreDataFeedCache
+	}
+
+	static func fetchRequest() -> NSFetchRequest<CoreDataFeedCache> {
+		return NSFetchRequest<CoreDataFeedCache>(entityName: "CoreDataFeedCache")
+	}
+}
