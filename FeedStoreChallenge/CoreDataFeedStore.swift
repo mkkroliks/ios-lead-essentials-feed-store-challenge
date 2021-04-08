6477f1d54b07d1a0a50fa03dc0ae3228ded7b2ea
Maciej Krolikowski <maciej.krolikowski@allegro.pl>
2021-04-08 12:47:25 +0200

    Refactor - this commit was previously splitted into smaller commits

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -6,8 +6,6 @@ import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
 	private static let modelName = "CoreDataFeedStore"
-	private let cacheEntityKey = "CoreDataFeedCache"
-	static let feedImageEntityKey = "CoreDataFeedImage"
 
 	private static let model = NSManagedObjectModel(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
 
@@ -32,14 +30,13 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		let request = NSFetchRequest<NSFetchRequestResult>(entityName: cacheEntityKey)
 		do {
-			let result = try context.fetch(request)
-			if let cache = result.first as? CoreDataFeedCache {
-				let feed = cache.feedItems?
-					.compactMap { $0 as? CoreDataFeedImage }
-					.map { $0.toLocal() }
-				completion(.found(feed: feed!, timestamp: cache.timestamp!))
+			if
+				let cache = try CoreDataFeedCache.fetch(context: context),
+				let feedItems = cache.feedItems,
+				let timestamp = cache.timestamp {
+				let feed = mapToLocalFeed(feedItems: feedItems)
+				completion(.found(feed: feed, timestamp: timestamp))
 			} else {
 				completion(.empty)
 			}
@@ -49,21 +46,14 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	private func deleteCache() throws {
-		let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: self.cacheEntityKey)
-		let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-		try container.persistentStoreCoordinator.execute(deleteRequest, with: context)
+		try container.persistentStoreCoordinator.execute(CoreDataFeedCache.deleteRequest(), with: context)
 		try context.save()
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		let cacheEntity = NSEntityDescription.entity(forEntityName: cacheEntityKey, in: self.context)
-		let cache = NSManagedObject(entity: cacheEntity!, insertInto: self.context) as! CoreDataFeedCache
-
 		do {
 			try deleteCache()
-			feed.forEach { cache.addToFeedItems($0.toEntity(context: context)) }
-			cache.timestamp = timestamp
-
+			try CoreDataFeedCache.insert(feed: feed, timestamp: timestamp, context: self.context)
 			try self.context.save()
 			completion(nil)
 		} catch {
@@ -79,24 +69,92 @@ public final class CoreDataFeedStore: FeedStore {
 			completion(error)
 		}
 	}
-}
 
-private extension CoreDataFeedImage {
-	func toLocal() -> LocalFeedImage {
-		LocalFeedImage(id: id!, description: descriptionText, location: location, url: url!)
+	private func mapToLocalFeed(feedItems: NSOrderedSet) -> [LocalFeedImage] {
+		feedItems
+			.compactMap { $0 as? CoreDataFeedImage }
+			.compactMap { $0.toLocal() }
 	}
 }
 
-private extension LocalFeedImage {
-	func toEntity(context: NSManagedObjectContext) -> CoreDataFeedImage {
-		let localFeedImageEntity = NSEntityDescription.entity(forEntityName: CoreDataFeedStore.feedImageEntityKey, in: context)
+extension LocalFeedImage {
+	enum Error: Swift.Error {
+		case entityNotFound
+		case managedObjectCreationFailure
+	}
 
-		let coreDataFeed = NSManagedObject(entity: localFeedImageEntity!, insertInto: context) as! CoreDataFeedImage
+	var feedImageEntityKey: String { "CoreDataFeedImage" }
+
+	func toEntity(context: NSManagedObjectContext) throws -> CoreDataFeedImage {
+		guard let localFeedImageEntity = NSEntityDescription.entity(forEntityName: self.feedImageEntityKey, in: context) else {
+			throw Error.entityNotFound
+		}
+		guard let coreDataFeed = NSManagedObject(entity: localFeedImageEntity, insertInto: context) as? CoreDataFeedImage else {
+			throw Error.managedObjectCreationFailure
+		}
+
 		coreDataFeed.id = id
 		coreDataFeed.descriptionText = description
 		coreDataFeed.location = location
 		coreDataFeed.url = url
 
 		return coreDataFeed
+	}
+}
+
+extension CoreDataFeedCache {
+	enum Error: Swift.Error {
+		case entityNotFound
+		case managedObjectCreationFailure
+	}
+
+	private static var cacheEntityKey: String { "CoreDataFeedCache" }
+
+	public static func createFetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+		return NSFetchRequest<NSFetchRequestResult>(entityName: self.cacheEntityKey)
+	}
+
+	static func deleteRequest() -> NSBatchDeleteRequest {
+		let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: self.cacheEntityKey)
+		return NSBatchDeleteRequest(fetchRequest: fetchRequest)
+	}
+
+	private static func create(context: NSManagedObjectContext) throws -> CoreDataFeedCache {
+		guard let cacheEntity = NSEntityDescription.entity(forEntityName: self.cacheEntityKey, in: context) else {
+			throw Error.entityNotFound
+		}
+		guard let cache = NSManagedObject(entity: cacheEntity, insertInto: context) as? CoreDataFeedCache else {
+			throw Error.managedObjectCreationFailure
+		}
+		return cache
+	}
+
+	static func insert(feed: [LocalFeedImage], timestamp: Date, context: NSManagedObjectContext) throws {
+		let cache = try create(context: context)
+		try cache.fill(with: feed, timestamp: timestamp, context: context)
+	}
+
+	private func fill(with feed: [LocalFeedImage], timestamp: Date, context: NSManagedObjectContext) throws {
+		try feed.forEach {
+			addToFeedItems(try $0.toEntity(context: context))
+		}
+		self.timestamp = timestamp
+	}
+
+	static func fetch(context: NSManagedObjectContext) throws -> CoreDataFeedCache? {
+		let result = try context.fetch(CoreDataFeedCache.createFetchRequest())
+		return result.first as? CoreDataFeedCache
+	}
+}
+
+extension CoreDataFeedImage {
+	func toLocal() -> LocalFeedImage? {
+		guard
+			let id = id,
+			let url = url
+		else {
+			return nil
+		}
+		return LocalFeedImage(id: id, description: descriptionText, location: location, url: url)
 	}
 }
